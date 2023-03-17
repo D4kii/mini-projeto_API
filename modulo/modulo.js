89d382f995251e25d02cf36e0caf8de82f9edc6d
Dani <danielalinodasilvaoliveira@gmail.com>
2023-03-17 17:01:10 -0300

    iniciando o arquivo das funções e fazendo testes - 17/03/2023

---
+++ modulo/modulo.js
@@ -0,0 +1,14 @@
+/**********************************************************************************************
+ * Objetivo: criar um Back-End para no futuro integrar em uma API que terá como 
+ * objetivo trazer Trazer contatos e conversas vindo de um número de celular.
+ * Autor: Daniela Lino
+ * Data: 17/03/2023
+ * Versão: 1.0
+ **********************************************************************************************/
+var whatsUsers = require('./contatos.js')
+const contatos = whatsUsers.whats-users.contacts
+
+const getContatos = function(filtro) {
+    const contatosJson 
+}
+getContatos()
